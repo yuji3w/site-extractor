7d08714874a73c23462facaed879bb92bc84063a
wangyuji431 <wangyuji431@gmail.com>
2018-08-15 21:32:48 -0400

    added password input fxn

---
+++ passwordPutter.py
@@ -0,0 +1,8 @@
+from splinter import Browser
+password2 = 'fakepassword'
+browser2 = Browser()
+browser2.visit('fakesite')
+browser2.find_by_name('Pass1').first.fill(password2)
+browser2.find_by_name('Submit0').first.click()
+print (browser2.html)
+browser2.quit()
